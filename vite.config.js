ab0d00650f04af402b52c840c63eed7d97b22fde
manolors <manolo.rs2005@gmail.com>
2023-10-20 15:57:42 +0100

    dummy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ const folderName = `${path.basename(process.cwd())}/`;
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
 const base = mode === "production" && isGitHubPages ? `/${folderName}` : "/";
 
+
 export default defineConfig({
   root: "src",
   base,
